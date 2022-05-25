98c94348cb345bdde6024f42aff430b2a00bc664
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2022-05-24 18:05:39 -0600

    I moved files out of that pointless folder

---
+++ Weapon.py
@@ -0,0 +1,9 @@
+class Weapon:
+    def __init__(self, Price, damageType, damage, name):
+        self.Price = Price
+        self.name = name
+        self.damage = damage
+        self.damageType = damageType
+    
+    def getData(self):
+        print(self)
